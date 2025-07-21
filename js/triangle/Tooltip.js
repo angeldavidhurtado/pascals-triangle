973ced85fbf2c91ba6ec0aeb83c8821fe8fcb9c7
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-07-21 14:51:04 -0500

    Tooltip touch resize

--- js/triangle/Tooltip.js
+++ js/triangle/Tooltip.js
@@ -5,22 +5,42 @@ class Tooltip {
 		this.tagTooltip = this.createTagTooltip()
 		document.addEventListener('mousemove', this.onHoverShowTooltip)
 		document.addEventListener('touchstart', this.handleTouchStart)
+
+		this.tagIsHidden = true
+		const tagTriangle = document.getElementById('triangle')
+		this.triangleRows = tagTriangle.childNodes
+		document.getElementById('add').addEventListener('click', () => this.showTooltip())
+		document.getElementById('delete').addEventListener('click', () => {
+			if (this.tag) {
+				const len = this.triangleRows.length
+				const lastRowTriangle = this.triangleRows[len-1]
+				if (lastRowTriangle == this.tag.parentNode) {
+					this.tag = null
+					this.tagTooltip.style.display = 'none'
+					this.tagIsHidden = true
+				}
+			}
+			setTimeout(this.showTooltip, 300)
+		})
+		window.addEventListener('resize', () => this.showTooltip())
 	}
 
 
 	onHoverShowTooltip = e => {
 		if (this.isTouchDevice()) return
 		const value = e.target?.value
+		this.tagIsHidden = true
 		if (!value) return this.tagTooltip.style.display = 'none'
-		this.showTooltip(value, e.pageX, e.pageY)
+		this.tagIsHidden = false
+		this.showTooltip(e.target)
 	}
 
 
 	handleTouchStart = e => {
 		const value = e.target?.value
 		if (!value) return
-		const touch = e.touches[0]
-		this.showTooltip(value, touch.pageX, touch.pageY)
+		this.tagIsHidden = false
+		this.showTooltip(e.target)
 	}
 
 
@@ -37,10 +57,21 @@ class Tooltip {
 	}
 
 
-	showTooltip = (value, x, y) => {
-		this.tagTooltip.innerText = value
+	showTooltip = (tag=null) => {
+		if (this.tagIsHidden) return
+		this.calculatePosition(tag)
+		this.tagTooltip.innerText = this.tag.value
 		this.tagTooltip.style.display = 'block'
-		this.tagTooltip.style.transform = `translate(${x}px, ${y - 30}px)`
+		this.tagTooltip.style.transform = `translate(${this.x}px, ${this.y}px)`
+	}
+
+
+	calculatePosition = (tag=null) => {
+		if (this.tagIsHidden) return
+		this.tag = tag ? tag : this.tag
+		const rect = this.tag.getBoundingClientRect()
+		this.x = rect.left + window.scrollX + 40
+		this.y = rect.top + window.scrollY + 48
 	}
 }
 
